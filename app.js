d10259d31ebcaaf01fbfc4524fe8efd011891741
Artem Kabanin <akabanin6@gmail.com>
2019-08-25 16:45:42 +0300

    Implement basic module pattern

---
+++ app.js
@@ -0,0 +1,26 @@
+const budgetController = (function () {
+    let x = 23;
+
+    const add = function (a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function (b) {
+            return add(b);
+        }
+    }
+})();
+
+const UIController = (function () {
+
+})();
+
+const controller = (function (budgetCtrl, UICtrl) {
+    const z = budgetCtrl.publicTest(10);
+    return {
+        anotherPublic: function () {
+            console.log('z', z);
+        }
+    }
+})(budgetController, UIController);
