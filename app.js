d83a931e5478174dcb4d8e071b90d353d81b57ce
Artem Kabanin <akabanin6@gmail.com>
2019-08-25 17:30:46 +0300

    Add event listeners

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
+// BUDGET CONTROLLER
 const budgetController = (function () {
-    let x = 23;
-
-    const add = function (a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function (b) {
-            return add(b);
-        }
-    }
+// some code
 })();
 
+// UI CONTROLLER
 const UIController = (function () {
+// some code
 
 })();
 
+// GLOBAL APP CONTROLLER
 const controller = (function (budgetCtrl, UICtrl) {
-    const z = budgetCtrl.publicTest(10);
-    return {
-        anotherPublic: function () {
-            console.log('z', z);
+    const addItemCtrl = function () {
+        // 1. Get the input data
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', () => {
+        addItemCtrl();
+    });
+
+    document.addEventListener('keydown', event => {
+        if (event.key === 'Enter') {
+            addItemCtrl();
         }
-    }
+    })
 })(budgetController, UIController);
