10b4fba76d77b8284c299ec39e962110ae6be99d
Artem Kabanin <akabanin6@gmail.com>
2019-08-26 20:14:50 +0300

    Add reading input data methods

--- app.js
+++ app.js
@@ -5,21 +5,43 @@ const budgetController = (function () {
 
 // UI CONTROLLER
 const UIController = (function () {
+    const DOMclasses = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
 // some code
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMclasses.inputType).value,
+                description: document.querySelector(DOMclasses.inputDescription).value,
+                value: document.querySelector(DOMclasses.inputValue).value,
+            }
+        },
+
+        getDOMclasses: function () {
+            return DOMclasses;
+        }
+    }
 
 })();
 
 // GLOBAL APP CONTROLLER
 const controller = (function (budgetCtrl, UICtrl) {
+    const DOM = UIController.getDOMclasses();
     const addItemCtrl = function () {
         // 1. Get the input data
+        const input = UIController.getInput();
+        console.log(input);
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
     };
 
-    document.querySelector('.add__btn').addEventListener('click', () => {
+    document.querySelector(DOM.inputBtn).addEventListener('click', () => {
         addItemCtrl();
     });
 
