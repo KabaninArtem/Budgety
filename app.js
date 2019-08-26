e3442497770ffb5e24c8259da62908b06a68ddba
Artem Kabanin <akabanin6@gmail.com>
2019-08-26 20:54:41 +0300

    Create expenses and incomes function constructor

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 // BUDGET CONTROLLER
 const budgetController = (function () {
-// some code
+    const Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    const Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    const data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+    };
+
 })();
 
 // UI CONTROLLER
@@ -11,7 +33,7 @@ const UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
     };
-// some code
+
     return {
         getInput: function () {
             return {
